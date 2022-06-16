6bc31f762f3726709d96e79cdb53a0244bed6b7d
honokasato <honi.19.ons@docomo.ne.jp>
2022-06-16 21:40:49 +0900

    honojka

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -4,17 +4,36 @@
   </div>
 
   <div class="naiyou">内容</div>
+  <textarea name="comment" v-model="naiyou"></textarea>
 
   <div class="name">ペンネーム</div>
-
-  <v-card-actions>
-    <v-btn class="toukoubotton">投稿</v-btn>
-  </v-card-actions>
+  <input type="text" name="name" size="15" v-model="penName" />
+  <button v-on:click="toukou">投稿</button>
 </template>
 
 <script>
 // JS の領域
-document.write('<img src="shikuziri.png">')
+import { collection, addDoc } from "firebase/firestore"
+// firebase.js で db として export したものを import
+import { db } from "@/firebase"
+export default {
+  data() {
+    return {
+      naiyou: "",
+      penName: "",
+    }
+  },
+  methods: {
+    toukou() {
+      addDoc(collection(db, "shikujiri"), {
+        text: this.naiyou,
+        penName: this.penName,
+      })
+      this.naiyou = ""
+      this.penName = ""
+    },
+  },
+}
 </script>
 
 <style>
@@ -23,8 +42,8 @@ document.write('<img src="shikuziri.png">')
 .display-1 {
   color: rgb(10, 10, 10);
   height: 100px;
-  display: table;
-  text-align: center;
+  display: flex;
+  justify-content: center;
 }
 .display-1 div {
   display: table-cell;
