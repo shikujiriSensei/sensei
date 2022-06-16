dbc608da3db7cc7305a43c43e176b654f94ee01a
honokasato <honi.19.ons@docomo.ne.jp>
2022-06-16 21:11:36 +0900

    “コミットメッセージ”

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,5 +1,46 @@
 <template>
-  <div class="post">
-    <h1>This is a post page</h1>
+  <div>
+    <h1 class="display-1">Post</h1>
   </div>
+
+  <div class="naiyou">内容</div>
+
+  <div class="name">ペンネーム</div>
+
+  <v-card-actions>
+    <v-btn class="toukoubotton">投稿</v-btn>
+  </v-card-actions>
 </template>
+
+<script>
+// JS の領域
+document.write('<img src="shikuziri.png">')
+</script>
+
+<style>
+/* CSS の領域 */
+
+.display-1 {
+  color: rgb(10, 10, 10);
+  height: 100px;
+  display: table;
+  text-align: center;
+}
+.display-1 div {
+  display: table-cell;
+  vertical-align: middle;
+}
+.naiyou {
+  color: rgb(10, 10, 10);
+  height: 100px;
+  display: table;
+  text-align: center;
+}
+
+.toukoubotton {
+  color: rgb(10, 10, 10);
+  height: 100px;
+  display: table;
+  text-align: center;
+}
+</style>
